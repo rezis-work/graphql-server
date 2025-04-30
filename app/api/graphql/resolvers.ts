3dd3d29c395e43186130d908145d0e030312bc8e
Rezi Karanadze <karanadzerevazi@gmail.com>
2025-04-30 17:38:55 +0400

    implement enum and union resolvers as well nested objects

--- app/api/graphql/resolvers.ts
+++ app/api/graphql/resolvers.ts
@@ -1,15 +1,35 @@
 const resolvers = {
+  SearchType: {
+    __resolveType(obj: { species: string }) {
+      if (obj.species) {
+        return "Animal";
+      }
+      return "Person";
+    },
+  },
   Person: {
     name: (person: { name: string }) => {
       return person.name.toUpperCase();
     },
+    pets: (person: { pets: { species: string; name: string }[] }) => {
+      return [{ species: "Dog", name: "Bobby" }];
+    },
   },
   Query: {
+    search: () => {
+      return [
+        { name: "Rezi", id: "baskbcasjkbak" },
+        {
+          name: "Bobby",
+          species: "Dog",
+        },
+      ];
+    },
     me: () => {
       return "me";
     },
     people: () => {
-      return [{ id: "acscasc", name: "Sam" }];
+      return [{ id: "acscasc", name: "Sam", pets: [1] }];
     },
   },
 };
--- app/api/graphql/schema.ts
+++ app/api/graphql/schema.ts
@@ -1,11 +1,21 @@
 const schema = `#graphql
+type Animal {
+  species: String!
+  name: String!
+}
+
 type Person {
   name: String!
   id: ID!
+  pets: [Animal]!
 }
+
+union SearchType = Animal | Person
+
 type Query {
   me: String!
   people: [Person!]!
+  search: [SearchType]!
 }
 `;
 
